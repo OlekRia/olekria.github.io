04f03761d84d553e508c28f019977f1175499069
OlekRia <alerya@gmail.com>
2020-04-16 19:29:27 +0300

    Bruin

---
+++ precache-manifest.a81c376c4f7584dbbd0c20dd3dfe7d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50ac60e6a29c99498e3b12948af203c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c9fcffa80706e3b9098",
+    "url": "/static/css/main.7dca8494.chunk.css"
+  },
+  {
+    "revision": "f1f338d594b94ff9fe81",
+    "url": "/static/js/2.9eccc03c.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/static/js/2.9eccc03c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9fcffa80706e3b9098",
+    "url": "/static/js/main.ee616793.chunk.js"
+  },
+  {
+    "revision": "c7abc16a78cc270e00fa",
+    "url": "/static/js/runtime-main.3679682e.js"
+  }
+]);
