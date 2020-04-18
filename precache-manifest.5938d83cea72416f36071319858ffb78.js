94a03b03034351005236d9c8d4c0f6bb63fedaf5
OlekRia <alerya@gmail.com>
2020-04-18 19:19:32 +0300

    Bruin Page: new version

---
+++ precache-manifest.5938d83cea72416f36071319858ffb78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed912b42df58b0d6f1ed1d4ca599a340",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efcec48f5d4b8e7605d3",
+    "url": "/static/css/main.b7f20ccf.chunk.css"
+  },
+  {
+    "revision": "bc5f9758c55d18234f27",
+    "url": "/static/js/2.30622512.chunk.js"
+  },
+  {
+    "revision": "1580278ee401c24eabc68037de267ba8",
+    "url": "/static/js/2.30622512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcec48f5d4b8e7605d3",
+    "url": "/static/js/main.e7d39420.chunk.js"
+  },
+  {
+    "revision": "c7abc16a78cc270e00fa",
+    "url": "/static/js/runtime-main.3679682e.js"
+  }
+]);
